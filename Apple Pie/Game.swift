66c9aea4f8842c86171440364957d6a524b93293
Gonzalo Gamez <takosaga@gmail.com>
2020-01-09 20:21:05 -0600

    added game.swift, with newRound and updateUI

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Gonzalo Gamez on 1/9/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -17,10 +17,23 @@ class ViewController: UIViewController {
     let incorrectMovesAllowed = 7
     var totalWins = 0
     var totalLosses = 0
+    var currentGame: Game!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
+    }
+    
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI(){
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
     
     @IBAction func buttonTapped(_ sender: UIButton){
